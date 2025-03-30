b8831337d1d7eaf39e2916fae865a3748fef1005
Mungai Munene <mungai.munene@gmail.com>
2025-03-30 18:16:38 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Vite configuration for a React app
 export default defineConfig({
+  base: '/',  // Ensure the base URL is correct, especially if your app is in a sub-directory
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure output is correct
-    target: 'esnext', // Ensure it's modern JS
-    rollupOptions: {
-      output: {
-        format: 'es',  // Make sure the format is 'es' for JavaScript modules
-      },
-    },
-  },
-  base: '/',  // Ensure proper routing for SPAs
-  server: {
-    port: 3000,
-  },
 });
