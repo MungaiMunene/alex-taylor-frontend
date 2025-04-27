14d15ff44d53fbae2b23f090c76613753d78a5f3
Mungai Munene <mungai.munene@gmail.com>
2025-04-27 18:47:17 +0300

    updated

--- src/Services/userProfileService.js
+++ src/Services/userProfileService.js
@@ -1,10 +1,9 @@
-// src/services/userProfileService.js
+// src/Services/userProfileService.js
 
 export async function fetchUserProfile() {
     const response = await fetch('http://127.0.0.1:5000/api/user-profile');
     if (!response.ok) {
       throw new Error('Failed to fetch user profile');
     }
-    const data = await response.json();
-    return data;
+    return await response.json();
   }
